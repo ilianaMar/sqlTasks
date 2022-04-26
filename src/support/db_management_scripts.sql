120f50f890473daff4757e4fcd03a14238acca12
Iliana Markova <iliana.markova@estafet.com>
2022-04-26 09:28:49 +0300

    add readme file

--- src/support/db_management_scripts.sql
+++ src/support/db_management_scripts.sql
@@ -8,4 +8,6 @@ DROP DATABASE orders_management;
 ALTER DATABASE orders_management RENAME TO orders_management_new;
 
 --Backup database orders_management
+docker exec -i sql-task  /bin/bash -c "PGPASSWORD=123456  pg_dump --username postgres orders_management"
+    > <desired_path>{/Users/ilianamarkova/Desktop/testDump/order_management.sql}
 
