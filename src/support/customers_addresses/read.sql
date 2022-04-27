4875bf615dbeb57a285ec398031e2a3bd7aed534
Iliana Markova <iliana.markova@estafet.com>
2022-04-27 15:19:28 +0300

    minor fix

--- src/support/customers_addresses/read.sql
+++ src/support/customers_addresses/read.sql
@@ -15,23 +15,23 @@ SELECT address, city  FROM public.customers_addresses WHERE country = 'Bulgaria'
 AND city NOT IN ('Sofia', 'Plovdiv', 'Varna');
 
 --get last 10 added customer addresses with a province and address filled, but without a state value
-SELECT * from public.customers_addresses
+SELECT * FROM public.customers_addresses
 WHERE province IS NOT NULL AND address IS NOT NULL
 AND state IS NULL
 ORDER BY address_id desc
 LIMIT 10;
 
 --get addresses that have 4-digit postal code that end with 0.
---Order the result alphabetically by country and city
+--Order the result alphabetically by country and city from A to Z
 
 SELECT address, country, city, postal_code
 FROM public.customers_addresses
 WHERE cast(postal_code as text) LIKE   '%1__8%'
-ORDER BY country  desc, city DESC;
+ORDER BY country ASC, city ASC;
 
 --get addresses that have 4-digit postal code that start with 1 and end with 8.
---Order the result alphabetically by country and city
+--Order the result alphabetically by country and city from Z to A
 SELECT address, country, city, postal_code
 FROM public.customers_addresses
 WHERE cast(postal_code as text) LIKE  '%0'
-ORDER BY country  desc, city DESC;
+ORDER BY country  DESC, city DESC;
