e1ce326dbe12186191b07f479fdc9ee6147de97e
Iliana Markova <iliana.markova@estafet.com>
2022-04-28 14:31:22 +0300

    implement task IMT-13

---
+++ src/support/create_products_inventory_table.sql
@@ -0,0 +1,11 @@
+--create table products_inventory in database orders_management
+CREATE TABLE public.products_inventory (
+    product_id serial PRIMARY KEY,
+    product_name VARCHAR(255) NOT NULL,
+    available_quantity INT NOT NULL,
+    product_type VARCHAR(255) NOT NULL,
+    price_without_vat DECIMAL(10, 2) NOT NULL,
+    price_with_vat DECIMAL(10, 2),
+    is_in_stock BOOLEAN NOT NULL,
+    warehouse INT NOT NULL
+);
